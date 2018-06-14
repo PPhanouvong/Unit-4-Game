3ebc48bb38894a2eb0c5de5fd2b4b772425e23b8
pphanouvong <pphanouvong1@gmail.com>
2018-06-14 10:00:38 -0400

    Added sounds and name capture

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,18 @@
+
+var name = prompt("Players name is ");
+alert("Hello " + name);
+$("#name").html(name);
+
 //Global variables
 var c1 = (Math.floor(Math.random() * (12 - 1) + 1));
-var c2 = (Math.floor(Math.random() * (12 - 1) + 1));
-var c3 = (Math.floor(Math.random() * (12 - 1) + 1));
-var c4 = (Math.floor(Math.random() * (12 - 1) + 1));
+var c2 = (Math.floor(Math.random() * (11 - 1) + 1));
+var c3 = (Math.floor(Math.random() * (10 - 1) + 1));
+var c4 = (Math.floor(Math.random() * (9 - 1) + 1));
 var win = 0;
 var lose = 0;
 var goal;
+var loss = new Audio("assets/images/Loss.wav");
+var wins = new Audio("assets/images/Win.wav");
 
 
 //Game Reset
@@ -13,9 +20,9 @@ function reset() {
 
     number = (Math.floor(Math.random() * (120 - 19) + 19));
     c1 = (Math.floor(Math.random() * (12 - 1) + 1));
-    c2 = (Math.floor(Math.random() * (12 - 1) + 1));
-    c3 = (Math.floor(Math.random() * (12 - 1) + 1));
-    c4 = (Math.floor(Math.random() * (12 - 1) + 1))
+    c2 = (Math.floor(Math.random() * (11 - 1) + 1));
+    c3 = (Math.floor(Math.random() * (10 - 1) + 1));
+    c4 = (Math.floor(Math.random() * (9 - 1) + 1))
     goal = 0;
 
     $(".displayNumber").html(number);
@@ -29,9 +36,9 @@ function start() {
 
     number = (Math.floor(Math.random() * (120 - 19) + 19));
     c1 = (Math.floor(Math.random() * (12 - 1) + 1));
-    c2 = (Math.floor(Math.random() * (12 - 1) + 1));
-    c3 = (Math.floor(Math.random() * (12 - 1) + 1));
-    c4 = (Math.floor(Math.random() * (12 - 1) + 1));
+    c2 = (Math.floor(Math.random() * (11 - 1) + 1));
+    c3 = (Math.floor(Math.random() * (10 - 1) + 1));
+    c4 = (Math.floor(Math.random() * (9 - 1) + 1));
     goal = 0;
 
     $(".displayNumber").html(number);
@@ -53,6 +60,7 @@ function start() {
 
         if (number === goal) {
 
+            wins.play();
             alert("Win");
             win++;
             $("#wins").html(win);
@@ -61,9 +69,10 @@ function start() {
         }
         else if (goal > number) {
 
+            loss.play();
             alert("Lose");
-            loss++;
-            $("#losses").html(loss);
+            lose++;
+            $("#losses").html(lose);
             $(".displayScore").html(0);
             reset();
         }
